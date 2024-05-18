4889cc394485512f55d8bd82f996112902f212b8
doha <dohaahi@naver.com>
2024-05-18 23:59:36 +0900

    fix: @Param 추가하여 에러 수정

--- src/main/java/together/together_project/repository/UserJpaRepository.java
+++ src/main/java/together/together_project/repository/UserJpaRepository.java
@@ -3,13 +3,14 @@ package together.together_project.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import together.together_project.domain.User;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.email = :email")
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(@Param("email") String email);
 
     @Query("select u from User u where u.nickname = :nickname")
-    Optional<User> findByNickname(String nickname);
+    Optional<User> findByNickname(@Param("nickname") String nickname);
 }
