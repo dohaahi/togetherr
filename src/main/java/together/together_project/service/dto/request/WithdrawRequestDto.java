036096d02054c32a7b8051b6d068aa4135fad260
doha <dohaahi@naver.com>
2024-05-22 01:01:17 +0900

    refactor: Valid annotation 변경

--- src/main/java/together/together_project/service/dto/request/WithdrawRequestDto.java
+++ src/main/java/together/together_project/service/dto/request/WithdrawRequestDto.java
@@ -2,11 +2,12 @@ package together.together_project.service.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record WithdrawRequestDto(
-        @NotNull
+
+        @NotBlank(message = "비밀번호를 입력하지 않았습니다.")
         String password
 ) {
 }
