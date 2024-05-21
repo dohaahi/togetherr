3c9cd85c0017370e527be5ca25e57ab86077eebc
doha <dohaahi@naver.com>
2024-05-22 01:02:16 +0900

    refactor: NotNull Valid 추가

--- src/main/java/together/together_project/service/dto/request/SignupRequestDto.java
+++ src/main/java/together/together_project/service/dto/request/SignupRequestDto.java
@@ -5,6 +5,7 @@ import static together.together_project.exception.ErrorCode.PASSWORD_REGEX;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import together.together_project.domain.User;
@@ -12,12 +13,15 @@ import together.together_project.domain.User;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SignupRequestDto(
 
-        @Pattern(regexp = EMAIL_REGEX, message = "잘못된 이메일 형식입니다.")
+        @NotNull(message = "잘못된 이메일 양식입니다.")
+        @Pattern(regexp = EMAIL_REGEX, message = "잘못된 이메일 양식입니다.")
         String email,
 
+        @NotNull(message = "닉네임은 1글자 이상으로 작성해주세요.")
         @Length(min = 1, message = "닉네임은 1글자 이상으로 작성해주세요.")
         String nickname,
 
+        @NotNull(message = "영문, 숫자, 특수문자 포함한 8자리 이상으로 작성해주세요.")
         @Pattern(regexp = PASSWORD_REGEX, message = "영문, 숫자, 특수문자 포함한 8자리 이상으로 작성해주세요.")
         String password,
 
