4dde46588708c4f922db18aab6c95708ef9505ca
doha <dohaahi@naver.com>
2024-05-22 01:01:40 +0900

    refactor: 생성자 추가

--- src/main/java/together/together_project/exception/CustomException.java
+++ src/main/java/together/together_project/exception/CustomException.java
@@ -1,11 +1,14 @@
 package together.together_project.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getDescription());
+        this.errorCode = errorCode;
+    }
 }
