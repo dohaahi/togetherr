e01e9252e90309374dae1b22060ce7ab7d614d16
doha <dohaahi@naver.com>
2024-05-25 20:55:33 +0900

    refactor: json의 key 표기법 변경

--- src/main/java/together/together_project/service/dto/TokenDto.java
+++ src/main/java/together/together_project/service/dto/TokenDto.java
@@ -1,5 +1,9 @@
 package together.together_project.service.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TokenDto(
         String accessToken
 ) {
