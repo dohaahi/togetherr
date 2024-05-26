71023ebaa838b10b39cb3fe66ce1e8dd5c4e4be1
doha <dohaahi@naver.com>
2024-05-26 23:22:54 +0900

    fix: jwtFilter 관련 예외 처리 수정

--- src/main/java/together/together_project/filter/JwtFilter.java
+++ src/main/java/together/together_project/filter/JwtFilter.java
@@ -1,7 +1,10 @@
 package together.together_project.filter;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
@@ -12,8 +15,8 @@ import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import together.together_project.exception.CustomException;
 import together.together_project.exception.ErrorCode;
+import together.together_project.exception.ErrorResponse;
 import together.together_project.security.JwtProvider;
 
 @Component
@@ -30,6 +33,8 @@ public class JwtFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
 
+        // TODO: 게시물 조회 path 처리
+
         // 회원가입, 로그인의 경우 필터 실행 X
         if (request.getRequestURI().startsWith(AUTH_URI)) {
             filterChain.doFilter(request, response);
@@ -37,15 +42,23 @@ public class JwtFilter extends OncePerRequestFilter {
         }
 
         // request에서 토큰 꺼내기
-        // TODO: try-catch 로 해결하기
-        String accessToken = resolveTokenFromRequest(request);
+        String accessToken = null;
 
-        jwtProvider.verifyAuthTokenOrThrow(accessToken);
+        try {
+            accessToken = resolveTokenFromRequest(response, request);
+            jwtProvider.verifyAuthTokenOrThrow(accessToken);
+        } catch (JwtException exception) {
+            jwtExceptionHandler(response, exception.getMessage());
+            return;
+        }
 
         filterChain.doFilter(request, response);
     }
 
-    private String resolveTokenFromRequest(HttpServletRequest request) {
+    private String resolveTokenFromRequest(
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) {
         Cookie[] cookies = request.getCookies();
         if (isEmpty(cookies)) {
             return null;
@@ -55,6 +68,28 @@ public class JwtFilter extends OncePerRequestFilter {
                 .filter(cookie -> cookie.getName().equals("accessToken"))
                 .map(Cookie::getValue)
                 .findFirst()
-                .orElseThrow(() -> new CustomException(ErrorCode.TOKEN_VALIDATE));
+                .orElseThrow(() -> new JwtException(ErrorCode.TOKEN_NOT_FOUND.getDescription()));
+    }
+
+    private void jwtExceptionHandler(HttpServletResponse response, String error) {
+        int statusCode = 403;
+
+        response.setStatus(statusCode);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        try {
+            String json = new ObjectMapper()
+                    .writeValueAsString(ErrorResponse.builder()
+                            .data(null)
+                            .error(error)
+                            .statusCode(statusCode)
+                            .build()
+                    );
+            response.getWriter().write(json);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
     }
 }
