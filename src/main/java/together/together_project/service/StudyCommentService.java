69b7c4fe1d90164c1248fd9cbbebaf06be4fc0f2
doha <dohaahi@naver.com>
2024-06-06 22:06:50 +0900

    feat: 스터디 게시글 댓글 생성 기능 구현

--- src/main/java/together/together_project/service/StudyCommentService.java
+++ src/main/java/together/together_project/service/StudyCommentService.java
@@ -1,4 +1,31 @@
 package together.together_project.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import together.together_project.domain.Study;
+import together.together_project.domain.StudyPostComment;
+import together.together_project.domain.User;
+import together.together_project.repository.StudyPostCommentRepositoryImpl;
+import together.together_project.service.dto.request.WriteCommentRequestDto;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class StudyCommentService {
+
+    private final StudyService studyService;
+    private final StudyPostCommentRepositoryImpl studyPostCommentRepository;
+
+    public StudyPostComment write(WriteCommentRequestDto request, Long studyId, User currentUser) {
+        Study study = studyService.getById(studyId);
+
+        StudyPostComment comment = StudyPostComment.builder()
+                .studyPost(study.getStudyPost())
+                .author(currentUser)
+                .content(request.content())
+                .build();
+
+        return studyPostCommentRepository.save(comment);
+    }
 }
