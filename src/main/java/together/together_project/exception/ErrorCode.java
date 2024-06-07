c60aa8ca5433ddda168cbf907817b6cbbe506585
doha <dohaahi@naver.com>
2024-06-07 22:00:50 +0900

    refactor: 예외 메세지 수정

--- src/main/java/together/together_project/exception/ErrorCode.java
+++ src/main/java/together/together_project/exception/ErrorCode.java
@@ -20,18 +20,17 @@ public enum ErrorCode {
     TOKEN_EXPIRED("유효기간이 만료된 토큰입니다.", 401),
 
     MAX_PEOPLE_UNDER_LIMIT("최대 인원은 2명 이상이여야 합니다.", 422),
-    STUDY_POST_NOT_FOUND("존재하지 않는 게시글입니다.", 404),
-    STUDY_POST_NOT_FOUND_FOR_DELETE("삭제할 게시글이 존재하지 않습니다.", 404),
-    POST_BUMP_PERIOD_EXCEPTION("끌어올리기는 이틀이 지나야 가능합니다.", 409),
-    STUDY_NOT_FOUND("존재하는 게시물이 없습니다.", 404),
+    STUDY_NOT_FOUND("존재하지 않는 스터디입니다.", 404),
     STUDY_IS_FULLED("이미 모집이 완료된 스터디입니다.", 400),
     STUDY_ALREADY_JOINED("이미 참여 신청한 스터디입니다.", 400),
+    POST_NOT_FOUND_FOR_DELETE("삭제할 게시글이 존재하지 않습니다.", 404),
+    POST_BUMP_PERIOD_EXCEPTION("끌어올리기는 이틀이 지나야 가능합니다.", 409),
+    POST_NOT_FOUND("존재하는 게시물이 없습니다.", 404),
     PARTICIPANTS_NOT_FOUND("참여 신청한 인원이 없습니다.", 404),
 
     COMMENT_NOT_FOUND("댓글이 존재하지 않습니다.", 404),
 
     UNAUTHORIZED_ACCESS("접근 권한이 없는 사용자입니다.", 403),
-
     INVALID_REQUEST("잘못된 요청건입니다.", 400),
     UNKNOWN_ERROR("알 수 없는 에러가 발생했습니다.", 500);
 
