de7ad4f34f2432c5cf4f1de95add2df4922b9812
doha <dohaahi@naver.com>
2024-06-11 00:29:24 +0900

    refactor: 요청에 대한 응답 dto 수정

--- src/main/java/together/together_project/service/dto/request/RespondToJoinRequestDto.java
+++ src/main/java/together/together_project/service/dto/request/RespondToJoinRequestDto.java
@@ -7,10 +7,10 @@ import jakarta.validation.constraints.NotNull;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record RespondToJoinRequestDto(
 
-        @NotNull(message = "잘못된 요청건입니다.")
+        @NotNull(message = "회원 아이디가 제출되지 않았습니다.")
         Long userId,
 
-        @NotNull(message = "잘못된 요청건입니다.")
-        boolean response
+        @NotNull(message = "응답이 제출되지 않았습니다.")
+        boolean isAccept
 ) {
 }
