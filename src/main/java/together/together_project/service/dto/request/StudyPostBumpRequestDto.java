87c4e402a1d688b3a27134caacc74d6097883970
doha <dohaahi@naver.com>
2024-06-25 20:51:25 +0900

    refactor: NotBlank 어노테이션으로 변경

--- src/main/java/together/together_project/service/dto/request/StudyPostBumpRequestDto.java
+++ src/main/java/together/together_project/service/dto/request/StudyPostBumpRequestDto.java
@@ -2,12 +2,13 @@ package together.together_project.service.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record StudyPostBumpRequestDto(
-        @NotNull(message = "끌어올리기 날짜가 입력되지 않았습니다.")
+
+        @NotBlank(message = "끌어올리기 날짜가 입력되지 않았습니다.")
         LocalDateTime refreshedAt
 ) {
 }
