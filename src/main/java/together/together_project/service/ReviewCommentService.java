5b85745753d40ee56d84a647f2b11aea739c8a85
doha <dohaahi@naver.com>
2024-07-01 12:59:56 +0900

    feat: 리뷰 댓글 생성 기능 구현

--- src/main/java/together/together_project/service/ReviewCommentService.java
+++ src/main/java/together/together_project/service/ReviewCommentService.java
@@ -1,4 +1,38 @@
 package together.together_project.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import together.together_project.controller.ReviewCommentCreateRequestDto;
+import together.together_project.domain.ReviewComment;
+import together.together_project.domain.ReviewPost;
+import together.together_project.domain.User;
+import together.together_project.exception.CustomException;
+import together.together_project.exception.ErrorCode;
+import together.together_project.repository.ReviewCommentRepositoryImpl;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ReviewCommentService {
+
+    private final ReviewPostService reviewPostService;
+
+    private final ReviewCommentRepositoryImpl reviewCommentRepository;
+
+    public ReviewComment writeComment(Long reviewId, ReviewCommentCreateRequestDto request, User user) {
+        ReviewPost review = reviewPostService.getReview(reviewId);
+
+        if (request.content().trim().isEmpty()) {
+            throw new CustomException(ErrorCode.EMPTY_CONTENT_ERROR);
+        }
+        
+        ReviewComment comment = ReviewComment.builder()
+                .author(user)
+                .reviewPost(review)
+                .content(request.content())
+                .build();
+
+        return reviewCommentRepository.save(comment);
+    }
 }
