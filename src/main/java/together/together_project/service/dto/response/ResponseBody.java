6167bfa99e8d7cc9646cfd467022c4b4924a7a64
doha <dohaahi@naver.com>
2024-07-03 01:20:41 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/together/together_project/service/dto/response/ResponseBody.java
+++ src/main/java/together/together_project/service/dto/response/ResponseBody.java
@@ -2,16 +2,11 @@ package together.together_project.service.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import org.springframework.validation.FieldError;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record ResponseBody(Object data, String error, int statusCode) {
-
-    public static ResponseBody from(FieldError fieldError) {
-
-        return new ResponseBody(
-                null,
-                fieldError.getDefaultMessage(),
-                fieldError.hashCode());
-    }
+public record ResponseBody(
+        Object data,
+        String error,
+        int statusCode
+) {
 }
