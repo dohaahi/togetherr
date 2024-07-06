4ca332c47fefe6da898ae7271e867fed24d3dab8
doha <dohaahi@naver.com>
2024-07-06 13:02:25 +0900

    refactor: snake case로 변형되도록 수정

--- src/main/java/together/together_project/service/dto/response/StudyPostLikeResponseDto.java
+++ src/main/java/together/together_project/service/dto/response/StudyPostLikeResponseDto.java
@@ -1,8 +1,11 @@
 package together.together_project.service.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.time.LocalDateTime;
 import together.together_project.domain.StudyPostLikeLink;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record StudyPostLikeResponseDto(
         Long id,
         Long userId,
